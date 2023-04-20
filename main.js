974c154ef2a18253377b1b168791979d384bbcfd
munazzanisar5 <munazzanisar5@gmail.com>
2023-04-21 00:35:17 +0500

    eid_mubarak

---
+++ main.js
@@ -0,0 +1,128 @@
+// window.scrollTo(xCoord, yCoord);
+document.body.onload= ()=>{
+
+
+
+// window.scrollTo({ top: 0, behavior: 'smooth' });
+
+
+
+
+// let image_three = document.querySelector(".image_three");
+// image_three.hidden=true
+//     setTimeout(() => {
+//         image_three.hidden=false
+//       }, 5000);
+
+      // let image_two = document.querySelector(".image_two");
+//       image_two.hidden=true
+//     setTimeout(() => {
+//         image_two.hidden=false
+//       }, 3000);
+
+   
+      // let my_img=document.querySelector(".my_img")
+      // setTimeout(()=>{
+      //   my_img.style.display="none";
+      // },1000)
+    
+      function slideOne(){
+        // let image_three = document.querySelector(".image_three");
+        // // image_three.style.visibility="hidden";
+        // setTimeout(() => {
+        //   image_three.style.visibility="visible";
+        //   // window.scrollBy(0, 1500)
+        // }, 1000);
+        
+       
+        let image_two = document.querySelector(".image_two");
+        // image_two.style.visibility="hidden";
+        setTimeout(() => {
+          image_two.style.display="block";
+          image_two.style. scrollBbehavior= "smooth";
+          // if(window.scrollBy(0, -1500)){
+            // window.scrollBy(0, -1000)
+            // }
+        },1000);
+       
+        
+      
+        
+        
+        let image_one = document.querySelector(".image_one ");
+        
+        // image_one.style.visibility="hidden";
+        setTimeout(() => {
+          image_one.style.display="block";
+          body.style.scrollBbehavior= "smooth";
+          window.scrollBy(0, -1900)
+        
+        }, 2000);
+      
+        // image_one.style.display="none";
+
+        
+        // image_two.style.display="none"
+
+      
+        // image_three.style.display="none"
+
+       
+      }
+      slideOne();
+      screenTop(0,0)
+
+      // function myfunction(){
+      //   if(window.scrollBy(0, 1500)){
+      //     window.scrollBy()
+      //   }
+
+      // }
+      // myfunction();
+      
+      
+
+    //   let image_two = document.querySelector(".image_two");
+    //   const timers =  setTimeout(() => {
+    //      if (image_two.style.visibility = "hidden") {
+    //         image_two.style.visibility = "visible";
+    //         image_two.style.transition= "0.1s ease-in-out";
+    //      }
+    //      }, 2000);
+     
+    //      timers();
+
+         
+      // let image_three = document.querySelector(".image_three");
+      //  setTimeout(() => {
+       
+      //    if (image_three.style.visibility = "hidden") {
+      //       image_three.style.visibility = "visible";
+      //       image_three.style.transition= "0.1s ease-in-out";
+      //    }
+      //    }, 5000);
+     
+      
+
+             
+    //   let image_one = document.querySelector(".image_one");
+    //   const time =  setTimeout(() => {
+           
+    //      if (image_one.style.visibility = "hidden") {
+    //         image_one.style.visibility = "visible";
+    //         image_one.style.transition= "0.1s ease-in-out";
+    //      }
+    //      }, 3000);
+     
+    //      time();
+       
+   
+    console.log( );
+
+
+    // const autoscroll=()=>{
+    //   window.scrollBy(0,400)
+    // }
+
+
+  }
